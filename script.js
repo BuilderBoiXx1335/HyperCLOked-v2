95e8e15047413396e6150c28162026d42c61164d
BuilderBoiXx1335 <Workatapizzaplace11@gmail.com>
2025-09-08 12:47:39 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+function launchProxy() {
+  const url = document.getElementById("urlInput").value;
+  const newTab = window.open();
+  const iframeHTML = `
+    <!DOCTYPE html>
+    <html>
+    <head><title>Proxy Tab</title></head>
+    <body style="margin:0">
+      <iframe src="${url}" style="width:100vw;height:100vh;border:none;"></iframe>
+    </body>
+    </html>
+  `;
+  newTab.document.open();
+  newTab.document.write(iframeHTML);
+  newTab.document.close();
+}
